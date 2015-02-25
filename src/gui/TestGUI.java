537b6d81b42b4bc248cafe10df35146ff6070f86
edwardsjones <edwards.jones.94@gmail.com>
2015-02-25 10:21:45 +0000

    GUI updated with last night's lab work

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -1,11 +1,30 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+
+import controller.Controller;
 
 public class TestGUI {
 
+	private Controller controller;
+	
+	public TestGUI(Controller controller){
+		this.controller = controller;
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run(){
+				display();
+			}
+		});
+	}
+	
+	/*
+	 * Controlled by controller now.
 	public static void main(String[] args){
 		
+			
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run(){
 				display();
@@ -13,8 +32,9 @@ public class TestGUI {
 		});
 		
 	}
+	*/
 	
-	private static void display(){
+	private void display(){
 		
 		JFrame frame = new JFrame();
 		frame.setTitle("Ad Auction Dashboard");
@@ -22,9 +42,9 @@ public class TestGUI {
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
 		
-		JPanel dataPane = new DataPanel();
-		JPanel chartsPane = new ChartsPanel();
-		JPanel metricsPane = new MetricsPanel();
+		JPanel dataPane = new DataPanel(this.controller);
+		JPanel chartsPane = new ChartsPanel(this.controller);
+		JPanel metricsPane = new MetricsPanel(this.controller);
 
 		tabbedPane.addTab("Data", dataPane);
 		tabbedPane.addTab("Charts", chartsPane);
@@ -42,10 +62,19 @@ public class TestGUI {
 
 class DataPanel extends JPanel{
 	
-	DataPanel(){
+	private JButton open;
+	JFileChooser fc;
+	private Controller controller;
+	
+	DataPanel(Controller controller){
 		
+		this.controller = controller;
 		this.add(new JTextArea(30, 50));
-		this.add(new JButton("Open"));
+		open = new JButton("Open");
+		
+		open.addActionListener(new DataListener(controller, this));
+		
+		this.add(open);
 		
 	}
 	
@@ -53,9 +82,8 @@ class DataPanel extends JPanel{
 
 class ChartsPanel extends JPanel{
 	
-	ChartsPanel() {
+	ChartsPanel(Controller controller) {
 		
-
 		this.add(new JButton("Test Button 1"));
 		this.add(new JButton("Button Two"));
 		
@@ -65,10 +93,37 @@ class ChartsPanel extends JPanel{
 
 class MetricsPanel extends JPanel{
 	
-	MetricsPanel(){
+	MetricsPanel(Controller controller){
 		
 		this.add(new JLabel("Metrics stuff"));
 		
+	}
+	
+}
+
+class DataListener implements ActionListener {
+
+	private Controller controller;
+	private JFileChooser fc;
+	private DataPanel dp;
+	
+	public DataListener(Controller controller, DataPanel dp) {
+		this.controller = controller;
+		this.dp = dp;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		/*
+		fc = new JFileChooser();
+		
+		int returnVal = fc.showOpenDialog(dp);
+		
+		if (returnVal == JFileChooser.APPROVE_OPTION){
+			String filePath = fc.getSelectedFile().getAbsolutePath();
+			controller.setFileLocation(filePath);
+		}
+		*/
 	}
 	
 }
